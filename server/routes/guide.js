82809b60cdcc1f10fa0c7b41b081b0d8b55820bb
luckyying <862024320@qq.com>
2018-04-23 01:40:55 +0800

    update welcome.html

--- server/routes/guide.js
+++ server/routes/guide.js
@@ -4,10 +4,13 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
+    res.sendFile(path.join(__dirname, '../views/blog.html'));
 });
 router.get('/readBlog', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
+    res.sendFile(path.join(__dirname, '../views/blog.html'));
+});
+router.get('/findBlog', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../views/welcome.html'));
 });
 
 module.exports = router;
