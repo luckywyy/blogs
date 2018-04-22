202b8af741d343b0f3c3b2fd04d7f58e248d2cb1
luckyying <862024320@qq.com>
2018-04-22 16:29:20 +0800

    Get some understanding

--- server/routes/guide.js
+++ server/routes/guide.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+router.get('/readBlog', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/writeBlog', function(req, res, next) {
+  res.send('writeBlog');
 });
 
 module.exports = router;
